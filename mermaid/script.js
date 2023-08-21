a21bc8fb3103311b882f11b11012ebc9130b9a9e
Salman Aziz <salman.aziz@nintex.com>
2023-08-21 21:00:37 +1000

    change

--- mermaid/script.js
+++ mermaid/script.js
@@ -9,19 +9,29 @@ function closePopup() {
     document.getElementById('inputPopup').style.display = 'none';
     mermaidCode = document.getElementById('mermaidInput').value;
     currentStep = 0;
+    document.getElementById('mermaidContainer').innerHTML = ""; // Clear previous diagram
 }
 
 function nextStep() {
     currentStep++;
+
     if (currentStep === 1) {
         // Initialize mermaid and display the initial diagram
         mermaid.initialize({ startOnLoad: false });
         mermaid.render('mermaidDiagram', mermaidCode, (svgCode) => {
             document.getElementById('mermaidContainer').innerHTML = svgCode;
         });
-        // Add your animations here for the initial display.
+        // Add your animations here for the initial display
+        let svgElements = document.querySelectorAll('#mermaidContainer svg *');
+        svgElements.forEach((el, index) => {
+            el.style.transition = "opacity 0.5s";
+            el.style.opacity = 0;
+        });
     } else {
-        // Add further animations or steps to complete the diagram
-        // This will depend on the complexity of your diagram and how you want to present it
+        // Gradually reveal the diagram elements
+        let svgElements = document.querySelectorAll('#mermaidContainer svg *');
+        if (currentStep - 2 < svgElements.length) {
+            svgElements[currentStep - 2].style.opacity = 1;
+        }
     }
 }
