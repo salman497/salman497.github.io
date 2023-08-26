2e86212ca5ef5b9c0a43bcfc109019d2fadca473
Salman Aziz <salman.aziz@nintex.com>
2023-08-27 08:25:38 +1000

    chore : added diagrams

--- mermaid/script.js
+++ mermaid/script.js
@@ -1,5 +1,24 @@
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+
 let currentStep = 0;
 let mermaidCode = "";
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    window.openPopup = function() {
+        openPopup();
+    };
+
+    window.closePopup = function() {
+        closePopup();
+    };
+
+    // If you also want to define the nextStep function:
+    window.nextStep = function() {
+        nextStep();
+    };
+});
+
 
 function openPopup() {
     document.getElementById('inputPopup').style.display = 'block';
@@ -17,8 +36,8 @@ function nextStep() {
 
     if (currentStep === 1) {
         // Initialize mermaid and display the initial diagram
-        mermaid.initialize({ startOnLoad: false });
-        mermaid.render('mermaidDiagram', mermaidCode, (svgCode) => {
+        mermaid.initialize({ startOnLoad: true });
+        mermaid.render('mermaidDiagram', `graph TD\nHello --> World`).then(({ svgCode }) => {
             document.getElementById('mermaidContainer').innerHTML = svgCode;
         });
         // Add your animations here for the initial display
