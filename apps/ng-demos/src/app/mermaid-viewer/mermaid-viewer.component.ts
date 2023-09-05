c81bb65cc45cedcfe112180f268ac947a888783d
Salman Aziz <salman.aziz@nintex.com>
2023-09-05 12:00:20 +1000

    chore: meraid

--- apps/ng-demos/src/app/mermaid-viewer/mermaid-viewer.component.ts
+++ apps/ng-demos/src/app/mermaid-viewer/mermaid-viewer.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+declare var Reveal: any;
+declare var mermaid: any;
 @Component({
   selector: 'mono-repo-mermaid-viewer',
   standalone: true,
@@ -8,4 +10,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './mermaid-viewer.component.html',
   styleUrls: ['./mermaid-viewer.component.css'],
 })
-export class MermaidViewerComponent {}
+export class MermaidViewerComponent implements OnInit {
+  
+  public mermaidCode = ` graph TD;
+  A-->B;
+  A-->C;
+  B-->D;
+  C-->D;`;
+  ngOnInit() {
+    Reveal.initialize();
+    mermaid.initialize({startOnLoad: true});
+  }
+}
