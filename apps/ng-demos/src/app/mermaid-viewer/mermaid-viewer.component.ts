5b7bdbc130926c28cca01e6680497fb49272d545
Salman Aziz <salman.aziz@nintex.com>
2023-09-15 22:13:57 +1000

    chore: updated

--- apps/ng-demos/src/app/mermaid-viewer/mermaid-viewer.component.ts
+++ apps/ng-demos/src/app/mermaid-viewer/mermaid-viewer.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-declare var Reveal: any;
-declare var mermaid: any;
+import Reveal from 'reveal.js';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+declare var $: any;
 @Component({
   selector: 'mono-repo-mermaid-viewer',
   standalone: true,
@@ -10,15 +12,41 @@ declare var mermaid: any;
   templateUrl: './mermaid-viewer.component.html',
   styleUrls: ['./mermaid-viewer.component.css'],
 })
-export class MermaidViewerComponent implements OnInit {
-  
+export class MermaidViewerComponent implements OnInit, AfterViewInit {
+  //constructor(private cd: ChangeDetectorRef) {}
   public mermaidCode = ` graph TD;
   A-->B;
   A-->C;
   B-->D;
   C-->D;`;
   ngOnInit() {
-    Reveal.initialize();
-    mermaid.initialize({startOnLoad: true});
+  //   let deck = new Reveal({
+  //     plugins: [ Markdown ]
+  //  })
+  //  deck.initialize();
+    // Reveal.initialize().then(() => {
+    //   mermaid.initialize({startOnLoad: true});
+    //   this.cd.detectChanges();
+    // });  
   }
+
+  ngAfterViewInit() {
+    let deck = new Reveal($('#revealDiv'));
+    deck.initialize({
+        plugins: [Markdown],
+        // hash: true,
+        embedded: true,
+        minScale: 1.0,
+        controls: true,
+        controlsTutorial: true,
+        keyboardCondition: 'focused',
+    });
+    // deck.configure({
+    //     keyboard: {
+    //         27: () => {
+    //             $('presentModal').hide();
+    //         }, // do Nothing when ESC is pressed
+    //     },
+    // });
+}
 }
