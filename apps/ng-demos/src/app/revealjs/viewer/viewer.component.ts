b0f3fb1b4789d836bc85b315ec44a2e6ef607ef3
Salman Aziz <salman.aziz@nintex.com>
2023-09-17 13:17:17 +1000

    chore : reveal + mermaid

--- apps/ng-demos/src/app/revealjs/viewer/viewer.component.ts
+++ apps/ng-demos/src/app/revealjs/viewer/viewer.component.ts
@@ -2,19 +2,43 @@
 import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import Reveal from 'reveal.js';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-declare var $: any;
+// @ts-ignore 
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+// @ts-ignore 
+import RevealMermaidPlugin from "reveal.js-mermaid-plugin";
+// @ts-ignore 
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
+// @ts-ignore 
+import RevealNotes from "reveal.js/plugin/notes/notes.esm.js";
+// @ts-ignore 
+import RevealSvgTimelineFragment from "reveal.js-svg-timeline-fragment";
+// @ts-ignore 
+import RevealAnimateFragments from "reveal.js-animate-fragments";
+// @ts-ignore 
+import RevealScriptFragment from "reveal.js-script-fragment";
 
+
+
+export const plugins = [
+  Markdown,
+  RevealMermaidPlugin,
+  RevealSvgTimelineFragment,
+  RevealNotes,
+  RevealScriptFragment,
+  RevealAnimateFragments,
+  RevealHighlight,
+];
+declare var $: any;
 @Component({
-  selector: 'mono-repo-viewer',
+  selector: 'mono-repo-viewer', 
   standalone: true,
   imports: [CommonModule],
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.css']
 })
+
 export class ViewerComponent  implements OnInit, AfterViewInit {
+  
   //constructor(private cd: ChangeDetectorRef) {}
   public mermaidCode = ` graph TD;
   A-->B;
@@ -35,7 +59,7 @@ export class ViewerComponent  implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     let deck = new Reveal($('#revealDiv'));
     deck.initialize({
-        plugins: [Markdown],
+        plugins,
         embedded: true,
         minScale: 1.0,
         controls: true,
