a57caad48c06d8dfdad8ca279d7a6d2112f83502
Salman Aziz <salman.aziz@nintex.com>
2023-09-24 17:29:46 +1000

    chore: close

--- apps/ng-demos/src/app/revealjs/revealjs.component.ts
+++ apps/ng-demos/src/app/revealjs/revealjs.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewEncapsulation} from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation} from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { RevealJsState } from './state/state';
 import * as AppActions from './state/actions';
 import { selectEditorContent } from './state/selector';
 import { tap } from 'rxjs';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'mono-repo-revealjs',
   templateUrl: './revealjs.component.html',
@@ -15,6 +16,7 @@ export class RevealjsComponent {
   editorVisible: boolean = false;
   toggleViewer: boolean = false;
   content$ = this.store.select(selectEditorContent);
+  @ViewChild('editorSidenav') editorSidenav!: MatSidenav;
 
   constructor(private store: Store<RevealJsState>) { }
 
@@ -24,5 +26,8 @@ export class RevealjsComponent {
   updateContent(value: string): void {
     this.store.dispatch(AppActions.updateEditorContent({ content: value }));
     this.toggleViewer = !this.toggleViewer;
+  }
+  closeSidenav() {
+    this.editorSidenav.close();
   }
 }
