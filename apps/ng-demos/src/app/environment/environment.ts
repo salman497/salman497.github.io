0bdf244f82f3d39a97543cd1b24b4bb6bff21e09
Salman Aziz <salman.aziz@nintex.com>
2023-09-29 21:23:27 +1000

    chore: revert too 4200

--- apps/ng-demos/src/app/environment/environment.ts
+++ apps/ng-demos/src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    hostURL: 'http://localhost:4299',
+    hostURL: 'http://localhost:4200',
     supabaseUrl: 'https://keopaarrwhppnzememsw.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtlb3BhYXJyd2hwcG56ZW1lbXN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUwOTQ1MTMsImV4cCI6MjAxMDY3MDUxM30.2Nv4KkM6EqtLGtLEadPkvQsG5o7-fLs3q2_gKVkecdg'
   };
