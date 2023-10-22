15bb3d542a4a147558f494067dbeb97be930df38
Salman Aziz <salman.aziz@nintex.com>
2023-10-22 13:57:20 +1100

    chore updated

--- apps/ng-demos/src/app/revealjs/editor/markdown/tui-editor/tui-editor.utils.ts
+++ apps/ng-demos/src/app/revealjs/editor/markdown/tui-editor/tui-editor.utils.ts
@@ -1,7 +1,12 @@
 import Editor from '@toast-ui/editor';
+import { ToolbarItemOptions } from '@toast-ui/editor/types/ui';
 
-
-export function initEditor(markdown: string, elm: HTMLDivElement, onLoad: () => void, onChange: () => void): Editor {
+export function initEditor(
+  markdown: string,
+  elm: HTMLDivElement,
+  onLoad: () => void,
+  onChange: () => void
+): Editor {
   const el = document.createElement('div');
   elm.appendChild(el);
   return new Editor({
@@ -11,7 +16,7 @@ export function initEditor(markdown: string, elm: HTMLDivElement, onLoad: () => 
         onLoad();
       },
       change: () => {
-        onChange()
+        onChange();
       },
     },
     // previewStyle: 'vertical',
@@ -21,22 +26,10 @@ export function initEditor(markdown: string, elm: HTMLDivElement, onLoad: () => 
     initialValue: markdown,
     height: '600px',
     // plugins: [customAddCodePlugin],
-    // toolbarItems: [
-    //   ['code', 'codeblock'],
-    //   // [{
-    //   //   el: this.createDropdown(), //
-    //   //   command: 'bold',
-    //   //   tooltip: 'Custom Bold',
-    //   //   name: 'bold'
-    //   // }
-    //   // ],
-    //   ['hr', 'quote'],
-    //   ['heading', 'bold', 'italic', 'strike'],
-    //   ['ul', 'ol', 'task', 'indent', 'outdent'],
-    //   ['table', 'image', 'link'],
-    // ],
+    toolbarItems: getToolbars(),
   });
 }
+
 
 
 export function scrollToHeading(
@@ -56,6 +49,68 @@ export function scrollToHeading(
   editor.setScrollTop(target.offsetTop);
 }
 
+function getToolbars(): (string | ToolbarItemOptions)[][] {
+  return [
+    ['code', 'codeblock'],
+    // [{
+    //   el: this.createDropdown(), //
+    //   command: 'bold',
+    //   tooltip: 'Custom Bold',
+    //   name: 'bold'
+    // }
+    // ],
+    ['hr', 'quote'],
+    ['heading', 'bold', 'italic', 'strike'],
+    ['ul', 'ol', 'task', 'indent', 'outdent'],
+    ['table', 'image', 'link'],
+  ];
+}
+
+// function createLastButton() {
+//   const button = document.createElement('button');
+
+//   button.className = 'toastui-editor-toolbar-icons last';
+//   button.style.backgroundImage = 'none';
+//   button.style.margin = '0';
+//   button.innerHTML = `<i>CC</i>`;
+//   button.addEventListener('click', () => {
+//     console.log(`click`);
+//     // executeCustomCommand(this.editor);
+//     // this.editor.exec('bold');
+//   });
+
+//   return button;
+// }
+// function createDropdown() {
+//   const dropdown = document.createElement('select');
+
+//   // Create 3 options for the dropdown
+//   const option1 = document.createElement('option');
+//   option1.value = 'item1';
+//   option1.textContent = 'Item 1';
+
+//   const option2 = document.createElement('option');
+//   option2.value = 'item2';
+//   option2.textContent = 'Item 2';
+
+//   const option3 = document.createElement('option');
+//   option3.value = 'item3';
+//   option3.textContent = 'Item 3';
+
+//   // Add options to the dropdown
+//   dropdown.appendChild(option1);
+//   dropdown.appendChild(option2);
+//   dropdown.appendChild(option3);
+
+//   // Optional: Add event listener for handling the dropdown change
+//   dropdown.addEventListener('change', (event) => {
+//     console.log(`Selected item:`, event);
+//     // executeCustomCommand(this.editor);
+//   });
+
+//   return dropdown;
+// }
+
 function getHeadingFromMarkdown(el: HTMLDivElement): NodeListOf<HTMLElement> {
   return el.querySelectorAll('.toastui-editor-md-thematic-break');
 }
@@ -66,51 +121,4 @@ function getHeadingFromWYSUSYG(): NodeListOf<HTMLElement> {
   );
   const hrElements: any = rootElement.querySelectorAll('div > hr');
   return hrElements;
-}
-
-
-export function createDropdown() {
-  const dropdown = document.createElement('select');
-
-  // Create 3 options for the dropdown
-  const option1 = document.createElement('option');
-  option1.value = 'item1';
-  option1.textContent = 'Item 1';
-
-  const option2 = document.createElement('option');
-  option2.value = 'item2';
-  option2.textContent = 'Item 2';
-
-  const option3 = document.createElement('option');
-  option3.value = 'item3';
-  option3.textContent = 'Item 3';
-
-  // Add options to the dropdown
-  dropdown.appendChild(option1);
-  dropdown.appendChild(option2);
-  dropdown.appendChild(option3);
-
-  // Optional: Add event listener for handling the dropdown change
-  dropdown.addEventListener('change', (event) => {
-    console.log(`Selected item:`, event);
-    // executeCustomCommand(this.editor);
-  });
-
-  return dropdown;
-}
-
-export function createLastButton() {
-  const button = document.createElement('button');
-
-  button.className = 'toastui-editor-toolbar-icons last';
-  button.style.backgroundImage = 'none';
-  button.style.margin = '0';
-  button.innerHTML = `<i>CC</i>`;
-  button.addEventListener('click', () => {
-    console.log(`click`);
-    // executeCustomCommand(this.editor);
-    // this.editor.exec('bold');
-  });
-
-  return button;
 }
