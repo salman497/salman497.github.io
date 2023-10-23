23e65d23d7f7052c3a9ea479acc52dbad958e628
Salman Aziz <salman.aziz@nintex.com>
2023-10-23 15:44:07 +1100

    chore: update to component

--- apps/ng-demos/src/app/revealjs/editor/markdown/tui-editor/editor-plugin.component.ts
+++ apps/ng-demos/src/app/revealjs/editor/markdown/tui-editor/editor-plugin.component.ts
@@ -1,17 +1,12 @@
 // dropdown.component.ts
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'editor-plugin',
   standalone: true,
-  template: `
-    <select (change)="onChange($event.target)">
-      <option value="" disabled selected>Select markdown</option>
-      <option value="**bold**">Bold</option>
-      <option value="_italic_">Italic</option>
-      <option value="## Heading 2">Heading 2</option>
-    </select>
-  `
+  templateUrl: './editor-plugin.component.html',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./editor-plugin.component.css']
 })
 export class EditorPluginComponent {
   @Output() markdownSelected = new EventEmitter<string>();
