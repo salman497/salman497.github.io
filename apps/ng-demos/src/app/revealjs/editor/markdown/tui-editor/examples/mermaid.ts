6db6cf944444a5ce43ccce9f86bdd48d1847a435
Salman Aziz <salman.aziz@nintex.com>
2023-11-04 05:25:06 +1100

    chore: added mermaid example

--- apps/ng-demos/src/app/revealjs/editor/markdown/tui-editor/examples/mermaid.ts
+++ apps/ng-demos/src/app/revealjs/editor/markdown/tui-editor/examples/mermaid.ts
@@ -195,8 +195,8 @@ sankey-beta
     markdown: ``
   },
   flowchartSteps: {
-    markdown: `<div><br class="ProseMirror-trailingBreak"></div><div class="toastui-editor-md-code-block-line-background start"><span class="toastui-editor-md-code-block"><span class="toastui-editor-md-delimiter">\`\`\`</span><span class="toastui-editor-md-meta">mermaid</span></span></div><div class="toastui-editor-md-code-block-line-background"><br class="ProseMirror-trailingBreak"></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">flowchart TD</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    A[Christmas] --&gt;|Get money| B(Go shopping)</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    B --&gt; C{Let me think}</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    C --&gt;|One| D[Laptop]</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    C --&gt;|Two| E[iPhone]</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    C --&gt;|Three| F[fa:fa-car Car]</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block"><span class="toastui-editor-md-delimiter">\`\`\`</span></span></div>`,
-    html: `<div data-language="mermaid" class="toastui-editor-ww-code-block"><pre><code data-language="mermaid-steps">
+    markdown: `<div><br class="ProseMirror-trailingBreak"></div><div class="toastui-editor-md-code-block-line-background start"><span class="toastui-editor-md-code-block"><span class="toastui-editor-md-delimiter">\`\`\`</span><span class="toastui-editor-md-meta">mermaid-steps</span></span></div><div class="toastui-editor-md-code-block-line-background"><br class="ProseMirror-trailingBreak"></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">flowchart TD</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    A[Christmas] --&gt;|Get money| B(Go shopping)</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    B --&gt; C{Let me think}</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    C --&gt;|One| D[Laptop]</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    C --&gt;|Two| E[iPhone]</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">    C --&gt;|Three| F[fa:fa-car Car]</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block"><span class="toastui-editor-md-delimiter">\`\`\`</span></span></div>`,
+    html: `<div data-language="mermaid-steps" class="toastui-editor-ww-code-block"><pre><code data-language="mermaid-steps">
 flowchart LR
     A[Christmas] -->|Get money| B(Go shopping)
     B --> C{Let me think}
@@ -205,4 +205,14 @@ flowchart LR
     C -->|Three| F[fa:fa-car Car]
 </code></pre></div>`,
   },
+  sequenceDiagramSteps: {
+    markdown: `<div><br class="ProseMirror-trailingBreak"></div><div class="toastui-editor-md-code-block-line-background start"><span class="toastui-editor-md-code-block"><span class="toastui-editor-md-delimiter">\`\`\`</span><span class="toastui-editor-md-meta">mermaid-steps</span></span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">sequenceDiagram</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">Alice-&gt;&gt;+John: Hello John, how are you?</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">Alice-&gt;&gt;+John: John, can you hear me?</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">John--&gt;&gt;-Alice: Hi Alice, I can hear you!</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block">John--&gt;&gt;-Alice: I feel great!</span></div><div class="toastui-editor-md-code-block-line-background"><span class="toastui-editor-md-code-block"><span class="toastui-editor-md-delimiter">\`\`\`</span></span></div>`,
+    html: `<div data-language="mermaid-steps" class="toastui-editor-ww-code-block"><pre><code data-language="mermaid-steps">
+sequenceDiagram
+    Alice->>+John: Hello John, how are you?
+    Alice->>+John: John, can you hear me?
+    John-->>-Alice: Hi Alice, I can hear you!
+    John-->>-Alice: I feel great!
+</code></pre></div>`,
+  }
 };
