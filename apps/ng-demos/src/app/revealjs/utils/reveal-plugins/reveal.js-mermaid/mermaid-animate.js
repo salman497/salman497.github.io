2a0aa01457b4784d50c4c6a8976470e3189c8516
Salman Aziz <salman.aziz@nintex.com>
2023-11-03 12:53:20 +1100

    chore: fixed bug

--- apps/ng-demos/src/app/revealjs/utils/reveal-plugins/reveal.js-mermaid/mermaid-animate.js
+++ apps/ng-demos/src/app/revealjs/utils/reveal-plugins/reveal.js-mermaid/mermaid-animate.js
@@ -5,29 +5,66 @@ export async function animateMermaid(el, textGraphDefinition) {
   //     return;
   // }
   if (el.classList.contains('mermaid-flowchart')) {
-    addFragmentsToFlowChart(el.firstElementChild);
+  //  addFragmentsToFlowChart(el.firstElementChild);
     const d = await mermaid.mermaidAPI.getDiagramFromText(textGraphDefinition);
     //  d.parser.yy.getVertices()
-     console.log('----diagram----', d.parser.yy.getVertices());
+     const vertices = d.parser.yy.getVertices();
+     const edges = d.parser.yy.getEdges();
+     console.log('----getVertices----', vertices);
+     console.log('----getEdges----',edges);
+     createFragments(el, edges, vertices);
+     
   }
 }
 
-function addFragmentsToFlowChart(graphElement) {
-
-  
-  const nodes = getAllNodes(graphElement);
-  const arrows = getAllArrows(graphElement);
-  const labels = getAllLabels(graphElement);
-  for (let i = 0; i < 100; i++) {
-    addFragmentToElement(nodes[i], i);
-    addFragmentToElement(arrows[i], i);
-    addFragmentToElement(labels[i], i);
-  }
+function createFragments(el, edges, vertices) {
+    const edgeLabels = el.querySelectorAll('span.edgeLabel');
+    let fragmentIndex = 0;
+    edges.forEach(item => {
+        if(item.start) {
+             // Start Node
+            const startNodeSelector = getNodeSelector(vertices[item.start].domId);
+            const startNodeElement = el.querySelector(startNodeSelector);
+            addFragmentToElement(startNodeElement, fragmentIndex);
+            fragmentIndex +=1;
+        }
+        if(item.end) {
+           // Label
+            const labelElement = getLabelElementByText(edgeLabels, item.text);
+           addFragmentToElement(labelElement, fragmentIndex);
+           // Arrow
+           const arrowSelector = getArrowSelector(item.start, item.end);
+           const arrowElement = el.querySelector(arrowSelector);
+           addFragmentToElement(arrowElement, fragmentIndex);
+           // End Node
+           const endNodeSelector = getNodeSelector(vertices[item.end].domId);
+           const endNodeElement = el.querySelector(endNodeSelector);
+           addFragmentToElement(endNodeElement, fragmentIndex);
+           fragmentIndex +=1;
+        }
+    });
 }
 
+function getLabelElementByText(edgeLabels, text) {
+    if(!text) {
+        return undefined;
+    }
+    return Array.from(edgeLabels).find(el => el.textContent.trim() === text);
+}
+
+
+function getNodeSelector(domId) {
+    const parts = domId.split('-');
+    parts.pop(); // Removes the last element from the array
+    return `[id^="${parts.join('-')}"]`;
+}
+
+function getArrowSelector(start, end) {
+    return `[id^="L-${start}-${end}"]`;
+}
 
 function addFragmentToElement(element, index) {
-  if (element) {
+  if (element && !element.hasAttribute('data-fragment-index')) {
     element.setAttribute('data-fragment-index', String(index));
     if (!element.classList.contains('fragment')) {
       element.classList.add('fragment');
@@ -38,46 +75,3 @@ function addFragmentToElement(element, index) {
   }
 }
 
-
-function getAllLabels(graphElement) {
-  let edgeLabelsParent = graphElement.querySelector('.edgeLabels');
-
-  // Initialize an array to hold the matching first child elements
-  let firstChildrenWithTransform = [];
-
-  if (edgeLabelsParent) {
-    // Use ':scope > *' to select all direct children
-    // Then find the first one with a 'transform' attribute
-    let children = edgeLabelsParent.querySelectorAll(':scope > *[transform]');
-    for (let child of children) {
-      // Add all children with a transform attribute
-      if (child.getAttribute('transform')) {
-        firstChildrenWithTransform.push(child);
-      }
-    }
-  }
-
-  return firstChildrenWithTransform;
-}
-
-
-function getAllArrows(graphElement) {
-  const arrows = graphElement.querySelectorAll('.flowchart-link');
-  return arrows;
-}
-
-function getAllNodes(graphElement) {
-  const arrows = graphElement.querySelectorAll('.flowchart-label');
-  return arrows;
-}
-
-
-function addFragmentByClassName(graphElement, className) {
-  const labels = graphElement.querySelectorAll(className);
-  labels.forEach((element, index) => {
-    element.setAttribute('data-fragment-index', String(index));
-    if (!element.classList.contains('fragment')) {
-      element.classList.add('fragment');
-    }
-  });
-}
