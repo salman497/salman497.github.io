1b6cfde1b0b929538af87e860735ee90aea276ce
salman497 <salmanaziz497@gmail.com>
2025-01-26 16:09:46 +1100

    chore: update

--- apps/evonix-site/webpack.config.js
+++ apps/evonix-site/webpack.config.js
@@ -1,19 +1,30 @@
 const { composePlugins, withNx } = require('@nx/webpack');
+const path = require('path');
 
 module.exports = composePlugins(withNx(), (config) => {
+  // Copy HTML files directly to output
+  config.module.rules.push({
+    test: /\.html$/,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          name: '[name].html',
+        },
+      },
+    ],
+    exclude: path.resolve(__dirname, 'src/index.html')
+  });
+
   config.devServer = {
     ...config.devServer,
     static: {
-      directory: config.output.path,
-    },
-    watchFiles: {
-      paths: ['src/**/*.html'], // adjust the path according to your project structure
-      options: {
-        usePolling: false,
+      directory: path.join(__dirname, 'src'),
+      watch: {
+        ignored: /node_modules/,
+        usePolling: true,
       },
     },
-    hot: true,
-    liveReload: true,
     setupMiddlewares: (middlewares, devServer) => {
       devServer.app.use((req, res, next) => {
         if (!req.path.includes('.')) {
@@ -24,6 +35,6 @@ module.exports = composePlugins(withNx(), (config) => {
       return middlewares;
     }
   };
-  
+
   return config;
 });
