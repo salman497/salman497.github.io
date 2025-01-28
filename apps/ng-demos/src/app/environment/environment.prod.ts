f519e682502a433c81a0fb3f0e504333a8e650d8
salman497 <salmanaziz497@gmail.com>
2025-01-28 16:59:45 +1100

    chore: updated

--- apps/ng-demos/src/app/environment/environment.prod.ts
+++ apps/ng-demos/src/app/environment/environment.prod.ts
@@ -4,13 +4,13 @@ export const environment = {
     chatDisabled: true,
     googleAnalyticId: 'G-3BDWL43VXZ',
     firebase: {
-      apiKey: "AIzaSyDlLBPmrctU5tkm4MjP-Jpo96wBoIoiyzo",
-      authDomain: "presenty-c21c5.firebaseapp.com",
-      projectId: "presenty-c21c5",
-      storageBucket: "presenty-c21c5.firebasestorage.app",
-      messagingSenderId: "332852709973",
-      appId: "1:332852709973:web:7db1436aea2884757fa2dc",
-      measurementId: "G-TQZE2BTQD1"
+      apiKey: "AIzaSyCtEIs3GMah9TlMbnO7S6hvJMcHzLMtxyg",
+      authDomain: "presenty-by-evonix.firebaseapp.com",
+      projectId: "presenty-by-evonix",
+      storageBucket: "presenty-by-evonix.firebasestorage.app",
+      messagingSenderId: "98157662769",
+      appId: "1:98157662769:web:d079d71cdf5802f83c3b10",
+      measurementId: "G-E7CB2MXKPE"
     }
   };
   
--- apps/ng-demos/src/app/environment/environment.ts
+++ apps/ng-demos/src/app/environment/environment.ts
@@ -2,13 +2,13 @@ export const environment = {
     production: false,
     hostURL: 'http://localhost:4200',
     firebase: {
-      apiKey: "AIzaSyDlLBPmrctU5tkm4MjP-Jpo96wBoIoiyzo",
-      authDomain: "presenty-c21c5.firebaseapp.com",
-      projectId: "presenty-c21c5",
-      storageBucket: "presenty-c21c5.firebasestorage.app",
-      messagingSenderId: "332852709973",
-      appId: "1:332852709973:web:7db1436aea2884757fa2dc",
-      measurementId: "G-TQZE2BTQD1"
+      apiKey: "AIzaSyCtEIs3GMah9TlMbnO7S6hvJMcHzLMtxyg",
+      authDomain: "presenty-by-evonix.firebaseapp.com",
+      projectId: "presenty-by-evonix",
+      storageBucket: "presenty-by-evonix.firebasestorage.app",
+      messagingSenderId: "98157662769",
+      appId: "1:98157662769:web:d079d71cdf5802f83c3b10",
+      measurementId: "G-E7CB2MXKPE"
     },
     chatDisabled: false,
     googleAnalyticId: 'G-MBSH5R8MXN'
