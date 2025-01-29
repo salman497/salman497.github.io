5d9d60fd349ba268c74beec73c93405e23c1de4b
salman497 <salmanaziz497@gmail.com>
2025-01-29 17:11:19 +1100

    chore: new google analystics id

--- apps/ng-demos/src/app/environment/environment.ts
+++ apps/ng-demos/src/app/environment/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   supabaseUrl: 'https://tdshfbcwufayaaiflple.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkc2hmYmN3dWZheWFhaWZscGxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4MDYyMTAsImV4cCI6MjA1MzM4MjIxMH0.wlfJrBdrnm_iEOPytYighCEELBk3fFi__sXXENlzGmU',
   chatDisabled: false,
-  googleAnalyticId: 'G-MBSH5R8MXN'
+  googleAnalyticId: 'G-2J2F0240J8'
 };
 
